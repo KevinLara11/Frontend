f18a95a3d952f99c062d2c6f5a75b7c66c4a1381
kevin97-png <133811075+kevin97-png@users.noreply.github.com>
2023-11-06 17:50:23 -0500

    Seccion Frontend

---
+++ src/Login.jsx
@@ -0,0 +1,24 @@
+/* import React from 'react' */
+/* import { faEnvelopes } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+function Login() {
+  return (
+    <div className="flex items-center justify-center flex-col h-screen bg-amber-100">
+        <div className="w-40 h-40 mb-5">
+          <img src="../public/logo.jpg" alt="logo" />
+        </div>
+        <div className="bg-white p-5 rounded-none shadow-md h-70 w-80">
+          <h2 className="text-x font-semibold-[#BDBDBD] mb-4 flex justify-center">Bienvenido ingresa con tu cuenta</h2>
+        </div>
+        <form method="post" action="../src/components/Dashboard.jsx">
+          <div className="flex items-center gap-3 h-10 max-w-[360px] border rounded-md border-[#BDBDBD] mb-4">
+            <input type="text" name="email" placeholder="Email" className="h-9 w-full outline-none"></input>
+            <FontAwesomeIcon icon={faEnvelopes} />
+          </div>
+        </form>
+    </div>
+  )
+}
+
+export default Login */
